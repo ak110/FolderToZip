cd192fb9712b04e04f359ddcaa742e710788684d
aki. <mark@aur.ll.to>
2013-07-04 19:57:49 +0900

    上書き出来ない問題の修正など

--- FolderToZip.cs
+++ FolderToZip.cs
@@ -1,6 +1,10 @@
-﻿using System.IO.Compression;
+﻿using System.IO;
+using System.IO.Compression;
 class Program {
     static void Main(string[] args) {
-        ZipFile.CreateFromDirectory(args[0], args[0] + ".zip", CompressionLevel.Optimal, true);
+        string srcDir = args[0];
+        string dstFile = srcDir.TrimEnd('\\', '/') + ".zip";
+        File.Delete(dstFile);
+        ZipFile.CreateFromDirectory(srcDir, dstFile, CompressionLevel.Optimal, true);
     }
 }
