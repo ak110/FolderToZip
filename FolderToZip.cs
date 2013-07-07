bb686d2080bcc82a69722fb7b53c4fb4ac77f26f
aki. <mark@aur.ll.to>
2013-07-07 13:57:39 +0900

    複数フォルダに対応。

--- FolderToZip.cs
+++ FolderToZip.cs
@@ -2,9 +2,10 @@
 using System.IO.Compression;
 class Program {
     static void Main(string[] args) {
-        string srcDir = args[0];
-        string dstFile = srcDir.TrimEnd('\\', '/') + ".zip";
-        File.Delete(dstFile);
-        ZipFile.CreateFromDirectory(srcDir, dstFile, CompressionLevel.Optimal, true);
+        foreach (var srcDir in args) {
+            string dstFile = srcDir.TrimEnd('\\', '/') + ".zip";
+            File.Delete(dstFile);
+            ZipFile.CreateFromDirectory(srcDir, dstFile, CompressionLevel.Optimal, true);
+        }
     }
 }
--- README.md
+++ README.md
@@ -3,6 +3,6 @@ FolderToZip
 
 フォルダをZIP圧縮するだけのコマンドラインツール。(.NET 4.5)
 
-使い方: FolderToZip.exe フォルダパス
+使い方: FolderToZip.exe フォルダパス...
 
 「(フォルダパス) + ".zip"」にフォルダ付きで圧縮したZIPファイルを作成します。
